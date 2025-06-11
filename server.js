c989f7b253717dbaa91fc023be972dae64fb7f8d
ye21iin <go_120@naver.com>
2025-06-11 23:13:18 +0900

    Add middleware setting for resolving cors error #2

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults({
   static: "public",
-  cors: true, // cors 활성화
 });
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
+
 server.listen(3000, () => {
   console.log("JSON Server is running on port 3000");
 });
